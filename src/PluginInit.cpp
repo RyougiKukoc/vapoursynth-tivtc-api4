0f87ccdd5f45107a8e122cad8ec91c8eedd0d8d1
RyougiKukoc <st_stars@163.com>
2026-09-16 09:49:57 +0800

    Align plugin version with release

--- src/PluginInit.cpp
+++ src/PluginInit.cpp
@@ -607,7 +607,7 @@ static void VS_CC tdecimateCreate(const VSMap *in, VSMap *out, void *userData, V
 
 
 VS_EXTERNAL_API(void) VapourSynthPluginInit2(VSPlugin *plugin, const VSPLUGINAPI *vspapi) {
-    vspapi->configPlugin("com.nodame.tivtc", "tivtc", "Field matching and decimation", VS_MAKE_VERSION(3, 5), VAPOURSYNTH_API_VERSION, 0, plugin);
+    vspapi->configPlugin("com.nodame.tivtc", "tivtc", "Field matching and decimation", VS_MAKE_VERSION(3, 6), VAPOURSYNTH_API_VERSION, 0, plugin);
     vspapi->registerFunction("TFM",
                  "clip:vnode;"
                  "order:int:opt;"
